b980434707dcb64338dddab47e8a5566ae5b3b53
yotam <yotam@fruitspec.com>
2022-04-17 10:36:07 +0300

    new file:   FruitSpec_YOLO4-ZED_pipeline.ipynb

--- video_yolov4.sh
+++ video_yolov4.sh
@@ -1,6 +1,6 @@
 
 
-./darknet detector demo ./cfg/coco.data ./cfg/yolov4.cfg ./yolov4.weights test50.mp4 -i 0 -thresh 0.25
+./darknet detector demo ../FS-ZED/data/obj.data ../FS-ZED/libdarknet/cfg/yolov4-tiny-citrus-pomegranate.cfg ../FS-ZED/models/yolov4-tiny-citrus-pomegranate.weights "/media/yotam/My Book/JAI_Ein-Vered/BA/8BA8B/output/rgb_video.avi" -i 0 -thresh 0.1 -out_filename "/media/yotam/My Book/JAI_Ein-Vered/BA/8BA8B/output/rgb_video_detected.avi"
 
 
 
